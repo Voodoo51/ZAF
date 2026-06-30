c1593b6a7fdef1c921143f836259e2208396094f
painTojn1 <cezary.bula33@gmail.com>
2026-06-30 14:32:40 +0200

    Resolved merge conflict and added new Routes

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -11,6 +11,9 @@ import { PaymentsView } from "./views/Payments";
 import { PropositionListView } from "./views/PropositionList";
 import { PropositionCreatorView } from "./views/PropositionCreator";
 import { PropositionDetailsView } from "./views/PropositionDetails";
+import { RegisterUserView } from "./views/RegisterUser";
+import { UsersView } from "./views/Users";
+import { PaymentsAdminView } from "./views/PaymentsAdmin";
 
 const Protected = ({ children }: { children: ReactNode }) => {
   const { user } = useAppContext();
@@ -31,9 +34,12 @@ export const Nav = () =>{
             <Route path="/creator" element={<Protected><CreatorView/></Protected>} />
           )}
           <Route path="/payment" element={<Protected><PaymentsView/></Protected>} />
+          <Route path="/payments/:userId" element={<Protected><PaymentsAdminView/></Protected>} />
           <Route path="/proposition" element={<Protected><PropositionListView/></Protected>} />
           <Route path="/proposition/create" element={<Protected><PropositionCreatorView/></Protected>} />
           <Route path="/proposition/:propositionId" element={<Protected><PropositionDetailsView/></Protected>} />
+          <Route path="/register" element={<Protected><RegisterUserView /></Protected>} />
+          <Route path="/users" element={<Protected><UsersView /></Protected>} />
         </Routes>
       </BrowserRouter>
   );
